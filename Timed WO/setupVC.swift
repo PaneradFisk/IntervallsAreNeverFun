30593c7199abbe8e76a9565ef92c4ce7e928407b
André <andre.bergvall@outlook.com>
2020-01-22 21:17:07 +0100

    first commitment

---
+++ Timed WO/setupVC.swift
@@ -0,0 +1,26 @@
+//
+//  setupVCViewController.swift
+//  Timed WO
+//
+//  Created by André Bergvall on 2020-01-22.
+//  Copyright © 2020 André Bergvall. All rights reserved.
+//
+
+import UIKit
+
+class setupVC: UIViewController{
+    
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.gray
+
+
+    }
+
+
+    
+}
